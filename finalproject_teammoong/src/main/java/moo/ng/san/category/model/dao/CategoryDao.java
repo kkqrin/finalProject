89c7d580c465729bc1e2801fc9f9297c4e154ffe
Anews01 <anews01@naver.com>
2023-03-31 11:09:14 +0900

    [명훈] product pac추가 및 업무공간 분리
    product pac set 추가

--- finalproject_teammoong/src/main/java/moo/ng/san/category/model/dao/CategoryDao.java
+++ finalproject_teammoong/src/main/java/moo/ng/san/category/model/dao/CategoryDao.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class CategoryDao {
 	@Autowired
-	private SqlSessionTemplate sqlsessoin;
+	private SqlSessionTemplate sqlSessoin;
 }
---
+++ finalproject_teammoong/src/main/java/moo/ng/san/product/model/vo/Product.java
@@ -0,0 +1,26 @@
+package moo.ng.san.product.model.vo;
+
+import java.util.ArrayList;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	private int productNo;
+	private int categoryNo;
+	private String productName;
+	private int productEa;
+	private int productPrice;
+	private int productStatus;
+	private int productCost;
+	private int gongguNumber;
+	private String productContent;
+	private int productDiscount;
+	/*
+	 * 파일이미지용 arraylist 추가예정
+	 * */
+}
