7b2e4704125db012da8494118db21e89a4b3bf14
Anews01 <anews01@naver.com>
2023-04-04 17:20:12 +0900

    [명훈]insertProduct 수정
    [명훈]insertProduct 수정

--- finalproject_teammoong/src/main/java/moo/ng/san/product/controller/ProductController.java
+++ finalproject_teammoong/src/main/java/moo/ng/san/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package moo.ng.san.product.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import moo.ng.san.product.model.service.ProductService;
 
@@ -119,7 +120,7 @@ public class ProductController {
 	
 	
 //	명훈작업공간 120~200
-
+	
 	@RequestMapping(value="/insertProductFrm.do")
 	public String insertProductFrm() {
 		return "product/insertProductFrm";
