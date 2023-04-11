372a05d7f1e7222ee53c6c9f90d1e75e422a6cf9
kkqrin <126125426+kkqrin@users.noreply.github.com>
2023-04-11 11:43:06 +0900

    무한스크롤 ajax
    더보기 버튼 클릭시 ajax로 상품 리스트 불러오기 작업중

--- finalproject_teammoong/src/main/java/moo/ng/san/product/model/vo/ProductPageData.java
+++ finalproject_teammoong/src/main/java/moo/ng/san/product/model/vo/ProductPageData.java
@@ -13,10 +13,14 @@ import moo.ng.san.category.model.vo.DetailCategory;
 @NoArgsConstructor
 public class ProductPageData {
 	private ArrayList<Product> list;
-	private ArrayList<DetailCategory> dCategoryList;
+	private ArrayList<DetailCategory> detailCategoryList;
 	// 무한 스크롤
 	private int start;
 	private int amount;
+	private int detailCategoryNo;
+	
 	// 상위 카테고리 번호, 이름
 	private Category category;
+	// 하위 카테고리
+	private DetailCategory detailCategory;
 }
