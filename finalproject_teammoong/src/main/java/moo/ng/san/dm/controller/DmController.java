bd3d24c873f388b60fe1be34a2987de3bb10065f
Janet <janet195@naver.com>
2023-04-19 15:40:20 +0900

    [홍미] 디엠 jsp 연결

--- finalproject_teammoong/src/main/java/moo/ng/san/dm/controller/DmController.java
+++ finalproject_teammoong/src/main/java/moo/ng/san/dm/controller/DmController.java
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DmController {
 
-	@RequestMapping(value = "/myPageDmReceive.do")
+	@RequestMapping(value = "/receiveDmList.do")
 	public String myPageDmReceive() {
-		return "dm/myPageDmReceive";
+		return "dm/receiveDmList";
 	}
 	
-	@RequestMapping(value = "/myPageDmSend.do")
+	@RequestMapping(value = "/sendDmList.do")
 	public String myPageDmSend() {
-		return "dm/myPageDmSend";
+		return "dm/sendDmList";
 	}
 	
-	@RequestMapping(value = "/myPageDmWriteFrm.do")
+	@RequestMapping(value = "/dmWriteFrm.do")
 	public String myPageDmWriteFrm() {
-		return "dm/myPageDmWriteFrm";
+		return "dm/dmWriteFrm";
 	}
 	
 }
--- finalproject_teammoong/src/main/webapp/WEB-INF/views/common/myPageMenu.jsp
+++ finalproject_teammoong/src/main/webapp/WEB-INF/views/common/myPageMenu.jsp
@@ -30,9 +30,9 @@
 				</div><!--one-menu-->
 				<div class="one-menu">
 					<a class="one-menu-title">쪽지함</a>
-					<a href="/myPageDmWriteFrm.do">쪽지 보내기</a>
-					<a href="/myPageDmReceive.do">받은 쪽지함(<span>0</span>)</a>
-					<a href="/myPageDmSend.do">보낸 쪽지함</a>
+					<a href="/dmWriteFrm.do">쪽지 보내기</a>
+					<a href="/sendDmList.do">받은 쪽지함(<span>0</span>)</a>
+					<a href="/receiveDmList.do">보낸 쪽지함</a>
 				</div><!--one-menu-->
 			</div><!--mypage-menu-->
 			<input type="hidden" name="memberNo" value="${sessionScope.m.memberNo }">
