ec6a370f7bf61d9361173197b461794c455cf27d
Anews01 <anews01@naver.com>
2023-04-20 14:19:39 +0900

    [명훈]커밋

--- finalproject_teammoong/src/main/webapp/resources/js/productView.js
+++ finalproject_teammoong/src/main/webapp/resources/js/productView.js
@@ -472,3 +472,28 @@ $(document).ready(function() {
                 });
             };
         }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        //리뷰관련
+
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+              var reader = new FileReader();
+              reader.onload = function(e) {
+                document.getElementById('preview').src = e.target.result;
+              };
+              reader.readAsDataURL(input.files[0]);
+            } else {
+              document.getElementById('preview').src = "";
+            }
+          }
+
+
+
+
+
+
+
+
+
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        
